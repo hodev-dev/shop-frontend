ce2058b784c1437d721167f8fdc10140a9a92b64
hodev-dev <hodev.dev@gmail.com>
2020-08-20 16:08:54 +0430

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './assets/css/tailwind.css';
 
 const App = () => {
   return (
-    <div className="h-64 w-64 bg-red-100">
+    <div className="flex-1 h-64 bg-red-100">
       <h1>frontend</h1>
     </div>
   );
