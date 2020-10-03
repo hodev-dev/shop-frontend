6b84b0e4bc23766156c7b03a19027cff5304fcda
hodev-dev <hodev.dev@gmail.com>
2020-10-03 18:16:03 +0330

    bug fixes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,32 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 const LOGO = require('../assets/img/logo/LOGO.svg');
 
 const Login = () => {
   axios.defaults.withCredentials = true;
 
-  useEffect(() => {
-    axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {
-      axios.get('http://localhost:8000/logout').then((logout: AxiosResponse) => {
-        console.log({ logout });
-      });
-      axios.post('http://localhost:8000/login/', { email: 'test@mail.com', password: '123456' }).then((loginResponse: AxiosResponse) => {
-        console.log({ loginResponse });
-        axios.get('http://localhost:8000/api/user/').then((users: AxiosResponse) => {
-          console.log({ users });
-        });
-      }).catch((err: AxiosError) => {
-        console.log({ err });
-      });
-    });
-  }, [])
+  const [_email, setEmail] = useState<string | null>('');
+  const [_password, setPassword] = useState<string | null>('');
+
+  const handleEmail = (event: React.FormEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  }
+
+  const handlePassword = (event: React.FormEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  }
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    await axios.get('http://localhost:8000/sanctum/csrf-cookie');
+    const login_request = await axios.post('http://localhost:8000/login/', { email: _email, password: _password });
+    console.log({ login_request });
+  }
+  const logOut = async () => {
+    const logout = await axios.get('http://localhost:8000/logout');
+    console.log(logout);
+  }
 
   return (
     <>
@@ -28,15 +34,18 @@ const Login = () => {
         <Header />
         <div dir={"rtl"} className={"flex flex-col items-center self-center justify-center w-2/6 h-auto p-5 mt-auto mb-auto ml-auto mr-auto rounded-lg bg-dark-200"}>
           <img className={"w-64 h-64 "} src={LOGO} alt="" />
-          <div className={"w-full mt-5"}>
-            <label className={"self-start text-2xl font-semibold text-white"} htmlFor="">Email</label>
-            <input dir={"ltr"} className={"w-full h-16 p-5 text-2xl text-white outline-none bg-dark-100"} type="text" />
-          </div>
-          <div className={"w-full mt-5"}>
-            <label className={"self-start text-2xl font-semibold text-white"} htmlFor="">Password</label>
-            <input dir={"ltr"} className={"w-full h-16 p-5 text-2xl text-white outline-none bg-dark-100"} type="text" />
-          </div>
-          <button className={"w-full h-16 mt-10 text-3xl font-bold text-white bg-indigo-600 rounded-lg outline-none hover:bg-indigo-700"}>Login</button>
+          <form onSubmit={handleLogin}>
+            <div className={"w-full mt-5"}>
+              <label className={"self-start text-2xl font-semibold text-white"} htmlFor="">Email</label>
+              <input onChange={handleEmail} dir={"ltr"} className={"w-full h-16 p-5 text-2xl text-white outline-none bg-dark-100"} type="text" />
+            </div>
+            <div className={"w-full mt-5"}>
+              <label className={"self-start text-2xl font-semibold text-white"} htmlFor="">Password</label>
+              <input onChange={handlePassword} dir={"ltr"} className={"w-full h-16 p-5 text-2xl text-white outline-none bg-dark-100"} type="password" />
+            </div>
+            <button className={"w-full h-16 mt-10 text-3xl font-bold text-white bg-indigo-600 rounded-lg outline-none hover:bg-indigo-700"}>Login</button>
+          </form>
+          <button onClick={logOut} className={"w-full h-16 mt-10 text-3xl font-bold text-white bg-indigo-600 rounded-lg outline-none hover:bg-indigo-700"}>logout</button>
         </div>
       </div>
     </>
